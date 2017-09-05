0d3573e6dd08ad14f984e6d634230558244651a8
Jonny Hofmann <jonny.hofmann@divtec.ch>
2017-09-05 14:44:37 +0100

    Add config

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const io = require('socket.io-client')
 const config = require('./config.json')
 const socket = io.connect(config.server, {reconnect: true})
 
-socket.on('connect', (socket) => {
+socket.on('connect', () => {
   console.log('Client : Connected')
+  test()
 })
+function test() {
+  console.log('enter test function')
+  socket.emit('raspberryPI', {name : config.name})
+  console.log('test function ended')
+}
